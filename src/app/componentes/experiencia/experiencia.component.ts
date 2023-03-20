01ae6d5bcda9284c19a23b23429e2def36713429
CarolinaCampi <carolinalcampi@gmail.com>
2023-03-20 20:27:32 -0300

    Link to JSON of proyectos and addition of Chart.js

--- src/app/componentes/experiencia/experiencia.component.ts
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
+
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
